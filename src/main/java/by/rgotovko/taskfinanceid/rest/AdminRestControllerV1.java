667d3703f533c332cffc013549b3fdeeb37109b3
Roman Gotovko <gotovkoromanhzby@gmail.com>
2018-06-24 02:50:50 +0300

    initial commit

---
+++ src/main/java/by/rgotovko/taskfinanceid/rest/AdminRestControllerV1.java
@@ -0,0 +1,9 @@
+package by.rgotovko.taskfinanceid.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/v1/admin")
+public class AdminRestControllerV1 {
+}
---
+++ src/main/java/by/rgotovko/taskfinanceid/rest/ClientRestControllerV1.java
@@ -0,0 +1,9 @@
+package by.rgotovko.taskfinanceid.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/client")
+public class ClientRestControllerV1 {
+}
