a2af709f7bb7988be2b1d0ebc6db54a2e4a85104
Roman Gotovko <gotovkoromanhzby@gmail.com>
2018-06-25 02:52:33 +0300

    added finAllByStatus method

--- src/main/java/by/rgotovko/taskfinanceid/repository/RequestRepository.java
+++ src/main/java/by/rgotovko/taskfinanceid/repository/RequestRepository.java
@@ -1,7 +1,11 @@
 package by.rgotovko.taskfinanceid.repository;
 
 import by.rgotovko.taskfinanceid.model.Request;
+import by.rgotovko.taskfinanceid.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RequestRepository extends JpaRepository<Request, Integer> {
+    List<Request> findAllByStatus(Status status);
 }
