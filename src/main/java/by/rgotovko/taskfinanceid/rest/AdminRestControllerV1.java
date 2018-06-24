841bcf0f6eaa4e39493d89df2b423440b35d6bf7
Roman Gotovko <gotovkoromanhzby@gmail.com>
2018-06-25 02:54:54 +0300

    added requests

--- src/main/java/by/rgotovko/taskfinanceid/rest/AdminRestControllerV1.java
+++ src/main/java/by/rgotovko/taskfinanceid/rest/AdminRestControllerV1.java
@@ -1,9 +1,80 @@
 package by.rgotovko.taskfinanceid.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import by.rgotovko.taskfinanceid.model.Request;
+import by.rgotovko.taskfinanceid.model.Status;
+import by.rgotovko.taskfinanceid.service.interfaces.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping(name = "/api/v1/admin")
+@RequestMapping(path = "/api/v1/admin/requests")
 public class AdminRestControllerV1 {
+
+    private final RequestService requestService;
+
+    @Autowired
+    public AdminRestControllerV1(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<List<Request>> getAllRequests() {
+        List<Request> requests = this.requestService.getAll();
+        if (requests.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(requests, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{requestId}/done", method = RequestMethod.PATCH,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Request> setStatusDone(@PathVariable Integer requestId) {
+        if (requestId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Request request = requestService.getById(requestId);
+        if (request == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        request.setStatus(Status.DONE);
+        this.requestService.save(request);
+        return new ResponseEntity<>(request, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{requestId}/reject", method = RequestMethod.PATCH,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Request> setStatusRejected(@PathVariable Integer requestId) {
+        if (requestId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Request request = requestService.getById(requestId);
+        if (request == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        request.setStatus(Status.REJECTED);
+        this.requestService.save(request);
+        return new ResponseEntity<>(request, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/statistics/done", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Map<String, Integer>> getDoneRequestsCount() {
+        return new ResponseEntity<>(Collections.singletonMap("done_requests",
+                this.requestService.getAllByStatus(Status.DONE).size()), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/statistics/rejected", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Map<String, Integer>> getRejectedRequestsCount() {
+        return new ResponseEntity<>(Collections.singletonMap("rejected_requests",
+                this.requestService.getAllByStatus(Status.REJECTED).size()), HttpStatus.OK);
+    }
+
 }
--- src/main/java/by/rgotovko/taskfinanceid/rest/ClientRestControllerV1.java
+++ src/main/java/by/rgotovko/taskfinanceid/rest/ClientRestControllerV1.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
-@RequestMapping(path = "/api/v1/client")
+@RequestMapping(path = "/api/v1/client/requests")
 public class ClientRestControllerV1 {
 
     private final RequestService requestService;
@@ -23,13 +23,27 @@ public class ClientRestControllerV1 {
         this.requestService = requestService;
     }
 
-    @RequestMapping(path = "register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Request> registerRequest(@RequestBody Request request){
-        if(request == null){
+    @RequestMapping(path = "/register", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Request> registerRequest(@RequestBody Request request) {
+        if (request == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         request.setStatus(Status.OPEN);
         request = this.requestService.save(request);
         return new ResponseEntity<>(request, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "/{requestId}/status", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Map<String, Status>> getStatusOfRequest(@PathVariable Integer requestId) {
+        if (requestId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Request request = requestService.getById(requestId);
+        if (request == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(Collections.singletonMap("status", request.getStatus()), HttpStatus.OK);
     }
 }
